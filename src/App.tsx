cd75c8db73ce9973dd7d1d4c6fc951bea0cf7619
Aziz Rmadi <azizrmadi@gmail.com>
2025-07-22 21:01:55 -0500

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import { ThemeProvider } from "./components/theme-provider";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <>
+        <div className="px-32 py-3 min-h-screen">
+          <Navbar></Navbar>
+          <section id="home">
+            <Hero></Hero>
+          </section>
+          <Projects></Projects>
+        </div>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import { ModeToggle } from "./mode-toggle";
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center">
+      <div className="text-2xl">Aziz Rmadi</div>
+      <div className="space-x-4 text-xl">
+        <a href="#home" className="hover:underline">
+          Home
+        </a>
+        <a href="#about" className="hover:underline">
+          About
+        </a>
+        <a href="#projects" className="hover:underline">
+          Projects
+        </a>
+        <a href="#contact" className="hover:underline">
+          Contact
+        </a>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
